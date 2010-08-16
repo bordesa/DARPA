918cfb5893a574cc45bc7fcb7628dde4c0428ac5
Joseph Turian <turian@gmail.com>
2010-08-16 16:49:44 -0400

    Small updates to scripts, for Python google protobuf installation

--- preprocessor_baseline_UdeM/script/install-prototype-buffers-locally.sh
+++ preprocessor_baseline_UdeM/script/install-prototype-buffers-locally.sh
@@ -11,6 +11,10 @@ make && make install
 # make the Python
 cd python
 python setup.py build
+python setup.py test
+mkdir -p ../../install//lib/python2.5/site-packages/
+PYTHONPATH=$PYTHONPATH:../../install/lib/python2.5/site-packages/ python setup.py install --prefix=../../install/
+python setup.py install --prefix=../../install/
 
 # Link the python lib up with our code
 cd ../../..
