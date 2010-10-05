05dd4a3106ad8ec8c264dc44163ff8eb6eec3127
Xavier Glorot <glorotxa@iro.umontreal.ca>
2010-10-05 18:23:00 -0400

    added an evaluation sh script

---
+++ preprocessor_baseline_UdeM/script/kfold-evaluation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+PROJECTROOT=`dirname $0`/..
+DATADIR=$PROJECTROOT
+
+KFOLD=10
+TRAINSIZE=10000
+SEED=777
+
+python $PROJECTROOT/src/DARPAEvaluation.py $KFOLD $TRAINSIZE $PROJECTROOT/preprocessed-opentable $PROJECTROOT/DLmodel $SEED
+
+
+
