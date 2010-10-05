4289c53cde688df7698bfc6a0e7afc8e323e8245
Xavier Glorot <glorotxa@iro.umontreal.ca>
2010-10-05 18:20:13 -0400

    added current configuration file

--- preprocessor_baseline_UdeM/script/preprocess-and-split-data.sh
+++ preprocessor_baseline_UdeM/script/preprocess-and-split-data.sh
@@ -3,22 +3,13 @@ set -e
 PROJECTROOT=`dirname $0`/..
 LIBLINEAR=$PROJECTROOT/lib/liblinear
 DATADIR=$PROJECTROOT
-DBDIR=/path/to/your/opentable/db
+DBDIR=/data/lisa5/DARPA/OpenTable/db
 
 # Generate the data (preprocessing and reducing the dictionary size to DICTSIZE)
+# and split it according to the index given in myTrainRestaurantsIds.dat
 # Note if DICTSIZE=0, the dictionary contains all the words
 DICTSIZE=5000
-python $PROJECTROOT/src/dbToBoW.py $DATADIR/testDict.txt $DATADIR/testData.feat $DICTSIZE $DBDIR/opentable.*.db
-# IMPORTANT COMMENT:
-# if you don't want to directly generate the libsvm file but rather
-# want to write the text of reviews (after preprocessing and filtering
-# to DICTSIZE) comment the line above and uncomment the one below.
-#python $PROJECTROOT/src/dbToText.py $DATADIR/testDict.txt $DATADIR/testData.feat $DICTSIZE $DBDIR/opentable.*.db
+python $PROJECTROOT/src/dbToBoW_DARPA.py $DATADIR/featDict.txt $DATADIR/preprocessed-opentable myTrainRestaurantsIds.dat $DICTSIZE $DBDIR/opentable.*.db
 
-# Split test and training set
-# Note: The following split corresponds to our current split at UdeM
-NUMTRAIN=373532
-NUMTEST=62254
-head -n $NUMTRAIN $DATADIR/testData.feat > $DATADIR/train.libsvm
-tail -n $NUMTEST  $DATADIR/testData.feat > $DATADIR/test.libsvm
+
 
